c103286b1175f7c9a2564c529b3474481c3969e6
patry.dud <patry.dud@gmail.com>
2022-04-09 14:53:17 +0200

    add Zadania rozwiazania

--- src/zadania/ZadanieProgiPodatkowe.java
+++ src/zadania/ZadanieProgiPodatkowe.java
@@ -1,7 +1,13 @@
 package zadania;
 
 import java.util.Scanner;
-
+/*
+Napisać program obliczający należny podatek dochodowy od osób fizycznych.
+Program ma pobierać od użytkownika dochód i po obliczeniu wypisywać na ekranie należny podatek.
+Podatek obliczany jest wg. następujących reguł:
+• do wlaczanie 85.528 podatek wynosi 18% podstawy minus 556,02 PLN,
+• od 85.528 podatek wynosi 14.839,02 zł + 32% nadwyżki ponad 85.528,00
+ */
 public class ZadanieProgiPodatkowe {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
