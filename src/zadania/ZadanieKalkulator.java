8ee84f5953f89b0f4fd0304d136b3b29de6c7f34
patry.dud <patry.dud@gmail.com>
2022-04-09 15:59:31 +0200

    add Zadania rozwiazania -  kalkulator

---
+++ src/zadania/ZadanieKalkulator.java
@@ -0,0 +1,30 @@
+package zadania;
+
+import java.util.Scanner;
+
+public class ZadanieKalkulator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj operacje");
+        String operacja = scanner.next();
+        System.out.println("Podaj liczby");
+        double liczba1= scanner.nextDouble();
+        double liczba2= scanner.nextDouble();
+        double wynik;
+
+        switch (operacja){
+            case "+": wynik = liczba1+liczba2;
+            break;
+            case "-": wynik = liczba1 - liczba2;
+            break;
+            case "*": wynik = liczba1*liczba2;
+            break;
+            case "/": wynik = liczba1 / liczba2;
+            break;
+            default:wynik = 0.0;
+            break;
+        }
+        System.out.println("Wynik to: "+ wynik);
+    }
+}
