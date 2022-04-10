21410bdfcd4272938466de2dd88442547257143b
patry.dud <patry.dud@gmail.com>
2022-04-10 15:24:47 +0200

    add petla while to zadanie tablica wielowymiarowe-sposob2

---
+++ src/tablice/TabliceWielowymiaroweExample2.java
@@ -0,0 +1,17 @@
+package tablice;
+
+import java.util.Scanner;
+
+public class TabliceWielowymiaroweExample2 {
+
+    public static void main(String[] args) {
+        int[][] tab = new int[][]{{1,1,1,1}, {2,2,2,2}, {3,3,3,3}};
+
+        for (int i=0; i<tab.length; i++){
+            for(int j =0; j<tab[i].length; j++){
+                System.out.print(tab[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+}
