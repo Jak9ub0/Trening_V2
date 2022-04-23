ebc181534ba2528f99f880ea351e8432fee3dda8
patrycja <paatt10i@gmail.com>
2022-04-23 13:58:57 +0200

    zadanie varargs

--- src/varargs/VarargsZadanie.java
+++ src/varargs/VarargsZadanie.java
@@ -1,5 +1,8 @@
 package varargs;
 
+import java.util.Arrays;
+import java.util.Iterator;
+
 /*
 Zadanie: Stworzcie metode  grupa parametrow typu string,
  nastepnie metoda ma printoweac konkatencaje slow przekazanych parametrem grupowym
@@ -7,12 +10,21 @@ Zadanie: Stworzcie metode  grupa parametrow typu string,
 public class VarargsZadanie {
 
     public static void main(String[] args) {
-        new VarargsZadanie().wypluj("maslo", "bulka","kwiatek","zupka");
+        new VarargsZadanie().wypluj2("maslo", "bulka","kwiatek","zupka");
     }
 
     void wypluj(String... numbers) {
+
         for (String number : numbers) {
             System.out.print(number + " ");
         }
-    }}
+    }
+
+    void wypluj2(String... numbers) {
+        Iterator<String> it = Arrays.stream(numbers).iterator();
+        while(it.hasNext()){
+            System.out.print(it.next() + " ");
+        }
+    }
+}
 
